9b132b98e7d1744ff1bceff347e7fd404491bc44
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2019-05-11 18:14:33 +0200

    sync extracting and processing from a uri is completed

--- LeetCrawler.Core/LeetCrawler.cs
+++ LeetCrawler.Core/LeetCrawler.cs
@@ -25,16 +25,12 @@ namespace LeetCrawler.Core
             byte[] urldata = await client.GetByteArrayAsync(url);
             return Encoding.Default.GetString(urldata);
         }
-        /// <summary>
-        /// Using regular expressions to find links
-        /// </summary>
-        /// <param name="url"></param>
-        /// <returns></returns>
+        
         public async Task<HashSet<string>> ExtractLinks (string url)
         {
             string content = await ReadURLContent(url);
             HashSet<string> links = new HashSet<string>();
-            MatchCollection matches =  Regex.Matches(content, @"(href|src)=\""(.*?)\"""); //[link references]
+            MatchCollection matches =  Regex.Matches(content, @"(href|src)=\""(.*?)\"""); 
             foreach(Match m in matches)
             {
                 if (string.IsNullOrEmpty(m.Value.Trim())) continue;
@@ -45,18 +41,22 @@ namespace LeetCrawler.Core
             }
             return links;
         }
-        internal string ProcessLink (string link)
+        private string ProcessLink (string link)
         {
             //Remove attribute names
             link=link.TrimBegining("href");
             link=link.TrimBegining("src");            
             link = link.Trim(new char[] { '=', '"',' ' });
             //Remove external links
-            link = (link.StartsWith("http")) ? string.Empty : link;
-            //Remove caching parameters in css links
-
+            link = (link.StartsWith("http") ||link.StartsWith("javascript:void") ||link.StartsWith(@"//cdn.")) ? string.Empty : link;
+            //Remove window location from link
+            if (link.Contains("#"))
+                link = link.Substring(0, link.IndexOf("#"));
+            //Remove caching parameters in css and js links
+            if (link.Contains("css?")|| link.Contains("js?"))
+                link = link.Substring(0, link.LastIndexOf("?"));
+            link = link.TrimEnd(new char[] { '/' });
             return link;
-
         }
 
 

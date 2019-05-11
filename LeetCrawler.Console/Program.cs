992f4a513f9362dbacdf0f42f2cd17feb8a48b67
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2019-05-11 17:56:19 +0200

    some link processing added

--- LeetCrawler.Console/Program.cs
+++ LeetCrawler.Console/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using static System.Console;
+using LeetCrawler.Core;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace LeetCrawler.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            string url = @"https://tretton37.com/";
+            Crawler crawler = new Crawler();
+            HashSet<string> links = await crawler.ExtractLinks(url);
+            foreach(string link in links)
+                System.Console.WriteLine(link);
+
+            ReadLine();
         }
     }
 }
--- LeetCrawler.Core/ProjectExtensions.cs
+++ LeetCrawler.Core/ProjectExtensions.cs
@@ -4,5 +4,15 @@ namespace LeetCrawler.Core
 {
     public static class ProjectExtensions
     {
+        public static string TrimBegining(this string targetString, string trimString)
+        {
+            if (string.IsNullOrEmpty(trimString)) return targetString;
+            string modifiedString = targetString;
+            while (modifiedString.StartsWith(trimString))
+            {
+                modifiedString = modifiedString.Substring(trimString.Length);
+            }
+            return modifiedString;
+        }
     }
 }
